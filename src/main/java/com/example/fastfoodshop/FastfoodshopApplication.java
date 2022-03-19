80cc76b052cf66dea3ce3607246334c49d93cc0d
caglar <chaglar.ronin@gmail.com>
2022-03-19 23:17:06 +0100

    init

---
+++ src/main/java/com/example/fastfoodshop/FastfoodshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastfoodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastfoodshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastfoodshop/FastfoodshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastfoodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
