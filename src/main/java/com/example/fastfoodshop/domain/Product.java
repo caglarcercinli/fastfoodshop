7797fcbd3b38362a9d751b1fe12aadd0054b879b
DoganHaluk <ha_look@yahoo.com>
2022-03-21 23:52:39 +0100

    product class created

---
+++ src/main/java/com/example/fastfoodshop/domain/Product.java
@@ -0,0 +1,34 @@
+package com.example.fastfoodshop.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    protected Product() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
