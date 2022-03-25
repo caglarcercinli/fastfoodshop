e19b4de8d3d1b9f06cabff60ea995d2423847768
DoganHaluk <ha_look@yahoo.com>
2022-03-25 19:43:54 +0100

    17-customer class created

---
+++ src/main/java/com/example/fastfoodshop/domain/Customer.java
@@ -0,0 +1,12 @@
+package com.example.fastfoodshop.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
