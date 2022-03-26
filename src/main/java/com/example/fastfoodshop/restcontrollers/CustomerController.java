7f2d5e5abe8faa0bff74e02c6dbbf38079b2b69d
DoganHaluk <ha_look@yahoo.com>
2022-03-26 02:17:16 +0100

    17-customer controller created

---
+++ src/main/java/com/example/fastfoodshop/restcontrollers/CustomerController.java
@@ -0,0 +1,29 @@
+package com.example.fastfoodshop.restcontrollers;
+
+import com.example.fastfoodshop.domain.Customer;
+import com.example.fastfoodshop.exceptions.CustomerNotFoundException;
+import com.example.fastfoodshop.services.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+class CustomerController {
+    private final CustomerService customerService;
+
+    CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("{id}")
+    Customer get(@PathVariable long id) {
+        return customerService.findById(id).orElseThrow(CustomerNotFoundException::new);
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    void customerNotFound() {
+    }
+}
