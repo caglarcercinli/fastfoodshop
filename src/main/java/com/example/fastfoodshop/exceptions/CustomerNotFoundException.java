fd65577c6a2ef9a4f5d256a6c11b0f69b48da2ab
DoganHaluk <68382949+DoganHaluk@users.noreply.github.com>
2022-03-27 10:03:00 +0200

    Revert "17 provide getcustomers"

--- src/main/java/com/example/fastfoodshop/exceptions/CustomerNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.example.fastfoodshop.exceptions;
-
-public class CustomerNotFoundException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
-}
--- src/test/resources/insertCustomer.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customers (name)
-VALUES ('test');
