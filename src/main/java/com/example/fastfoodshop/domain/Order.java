dfcfd98214b15cb21435f8fb4eed44554c9c508f
caglarcercinli <59973993+caglarcercinli@users.noreply.github.com>
2022-04-30 17:05:27 +0200

    Update Order.java
    comments deleted

--- src/main/java/com/example/fastfoodshop/domain/Order.java
+++ src/main/java/com/example/fastfoodshop/domain/Order.java
@@ -11,8 +11,5 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-//    @JoinColumn(name = "customer_id")
-//    private Customer customer;
     private long customer_id;
 }
