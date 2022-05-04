4c38eb2ce2b265723ac51bb429cb7c4944aa06c0
caglarcercinli <chaglar.ronin@gmail.com>
2022-05-04 20:57:21 +0200

    45 - with joe

--- src/main/java/com/example/fastfoodshop/security/SecurityConfig.java
+++ src/main/java/com/example/fastfoodshop/security/SecurityConfig.java
@@ -7,39 +7,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+@EnableWebSecurity
+//@KeycloakConfiguration
+class SecurityConfig extends WebSecurityConfigurerAdapter /* extends KeycloakWebSecurityConfigurerAdapter */{
+    private static final String MANAGER = "manager";
+    private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
+    private static final String MAGAZIJNIER = "magazijnier";
 
-@KeycloakConfiguration
-class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-    @Autowired
-    public void configureGlobal(
-            AuthenticationManagerBuilder auth) throws Exception {
-
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-                = keycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-                new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("joe").password("{noop}theboss")
+                .authorities(MANAGER);
     }
 
-    @Bean
     @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(
-                new SessionRegistryImpl());
+    public void configure(WebSecurity web) throws Exception {
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.authorizeRequests()
-                .antMatchers("/customers*")
-                .hasRole("manager")
-                .anyRequest()
-                .permitAll();
-        http.cors().and().csrf().disable();
+        http.formLogin();
+        http.authorizeRequests(requests -> requests
+                .mvcMatchers("/customers")
+                .hasAuthority(MANAGER)
+        );
     }
+//    @Autowired
+//    public void configureGlobal(
+//            AuthenticationManagerBuilder auth) throws Exception {
+//
+//        KeycloakAuthenticationProvider keycloakAuthenticationProvider
+//                = keycloakAuthenticationProvider();
+//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
+//                new SimpleAuthorityMapper());
+//        auth.authenticationProvider(keycloakAuthenticationProvider);
+//    }
+//
+//    @Bean
+//    @Override
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//        return new RegisterSessionAuthenticationStrategy(
+//                new SessionRegistryImpl());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//        http.authorizeRequests()
+//                .antMatchers("/customers*")
+//                .hasRole("manager")
+//                .anyRequest()
+//                .permitAll();
+//        http.cors().and().csrf().disable();
+//    }
 }
