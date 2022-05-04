1f8a7712afd258a0ad378508d7b7bcc74cf4fd05
caglarcercinli <chaglar.ronin@gmail.com>
2022-05-04 21:17:05 +0200

    45 - corrections

--- src/main/java/com/example/fastfoodshop/security/SecurityConfig.java
+++ src/main/java/com/example/fastfoodshop/security/SecurityConfig.java
@@ -1,22 +1,13 @@
 package com.example.fastfoodshop.security;
 
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
 @EnableWebSecurity
-//@KeycloakConfiguration
-class SecurityConfig extends WebSecurityConfigurerAdapter /* extends KeycloakWebSecurityConfigurerAdapter */{
+class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String MANAGER = "manager";
     private static final String HELPDESKMEDEWERKER = "helpdeskmedewerker";
     private static final String MAGAZIJNIER = "magazijnier";
@@ -40,32 +31,4 @@ class SecurityConfig extends WebSecurityConfigurerAdapter /* extends KeycloakWeb
                 .hasAuthority(MANAGER)
         );
     }
-//    @Autowired
-//    public void configureGlobal(
-//            AuthenticationManagerBuilder auth) throws Exception {
-//
-//        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-//                = keycloakAuthenticationProvider();
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-//                new SimpleAuthorityMapper());
-//        auth.authenticationProvider(keycloakAuthenticationProvider);
-//    }
-//
-//    @Bean
-//    @Override
-//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-//        return new RegisterSessionAuthenticationStrategy(
-//                new SessionRegistryImpl());
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
-//        http.authorizeRequests()
-//                .antMatchers("/customers*")
-//                .hasRole("manager")
-//                .anyRequest()
-//                .permitAll();
-//        http.cors().and().csrf().disable();
-//    }
 }
