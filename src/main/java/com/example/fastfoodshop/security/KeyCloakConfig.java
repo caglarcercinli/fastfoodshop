e80da3036e1d19e8f97b0577e9d82af97ba4131f
caglarcercinli <chaglar.ronin@gmail.com>
2022-05-04 21:03:08 +0200

    45 - with joe

--- src/main/java/com/example/fastfoodshop/security/KeyCloakConfig.java
+++ src/main/java/com/example/fastfoodshop/security/KeyCloakConfig.java
@@ -4,11 +4,11 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class KeyCloakConfig {
-//    @Bean
-//    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-//        return new KeycloakSpringBootConfigResolver();
-//    }
-//}
+@Configuration
+public class KeyCloakConfig {
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
 
