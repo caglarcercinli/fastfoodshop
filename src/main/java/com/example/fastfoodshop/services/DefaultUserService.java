cdf1c1e69ffd0107c2d1d46dc7535b640a4b6e9c
caglarcercinli <chaglar.ronin@gmail.com>
2022-06-21 21:32:10 +0200

    54 - security ignored

--- src/main/java/com/example/fastfoodshop/services/DefaultUserService.java
+++ src/main/java/com/example/fastfoodshop/services/DefaultUserService.java
@@ -5,25 +5,18 @@ import com.example.fastfoodshop.domain.User;
 import com.example.fastfoodshop.repositories.RoleRepository;
 import com.example.fastfoodshop.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
-public class DefaultUserService implements UserService, UserDetailsService {
+public class DefaultUserService implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public Optional<User> findById(long id) {
@@ -57,13 +50,5 @@ public class DefaultUserService implements UserService, UserDetailsService {
     public User get(String username) {
         return userRepository.findByUsername(username);
     }
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("user not found");
-        }
-        List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
-    }
+
 }
